ce1bb4fce9cfb481523dae47ac357752a9ee547b
johnywu <johnywu@protonmail.com>
2022-05-09 15:56:53 +0700

    using Widget <View>

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: Center(
-              child: Text(
-            "First Flutter App",
-            style: TextStyle(color: Colors.red, fontFamily: "DancingScript"),
-          )),
-          actions: const [
-            InkWell(child: Icon(Icons.settings)),
-          ],
-        ),
-        body: Text(
-          'Flutter Hello',
-          style: TextStyle(
-            color: Colors.red,
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.red[200],
+                border: Border.all(width: 2, color: Colors.blue),
+                borderRadius: BorderRadius.all(Radius.circular(150))),
+            child: Center(
+                child: Text(
+              "Hello World",
+              style: TextStyle(color: Colors.white, fontSize: 40),
+            )),
           ),
         ),
       ),
